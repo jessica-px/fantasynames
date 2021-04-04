fc67c6aa9af19dc807e0cbc71e2936769898d7d3
Jessica Peck <jessypeck@gmail.com>
2021-04-04 11:50:28 -0700

    Use Language class for human Adds gender functionality

--- fantasynames/__init__.py
+++ fantasynames/__init__.py
@@ -3,4 +3,4 @@ from fantasynames.elf import elf
 from fantasynames.french import french
 from fantasynames.anglo import anglo
 from fantasynames.dwarf import dwarf
-from fantasynames.human_diverse import generate_human_name as human
+from fantasynames.human_diverse import human
