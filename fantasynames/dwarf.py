34b07fb6dd1e4b39b0256ec53d536cdaecddb024
Jessica Peck <jessypeck@gmail.com>
2021-04-03 21:04:37 -0700

    Add gender logic for dwarf first names

--- fantasynames/dwarf.py
+++ fantasynames/dwarf.py
@@ -9,22 +9,45 @@ import random
 transform_dwarf = define_transform_function(dwarf_data["transformations"])
 
 
-def gen_dwarf_name1() -> str:
+def gen_dwarf_name1_base() -> str:
     """
-    Outputs a randomized "dwarf" first name. Example outputs:
-    'Thormer', 'Hothvirda', 'Burmir', 'Ingvola', 'Einrom'
+    Outputs a randomized (masculine) "dwarf" first name.
     """
     name = gen_from_table(dwarf_data["name1_col1"], dwarf_data["name1_col2"])
-    # 30% chance of adding a suffix
-    if random.random() * 100 < 30:
-        name += random.choice(dwarf_data["name1_suffixes"])
+    return name
+
+
+def generate_dwarf_name1_female() -> str:
+    """
+    Outputs a randomized "dwarf" first name with a feminine suffix.
+    """
+    name = gen_dwarf_name1_base() + random.choice(dwarf_data["name1_suffixes"])
+    return name
+
+
+def gen_dwarf_name1(gender: str) -> str:
+    """
+    Outputs a randomized "dwarf" first name with variable gender.
+    """
+    if gender == "female":
+        name = generate_dwarf_name1_female()
+    elif gender == "male":
+        name = gen_dwarf_name1_base()
+    elif gender == "any":  # 50% chance of male or female first name
+        if random.random() * 100 < 50:
+            name = gen_dwarf_name1_base()
+        else:
+            name = generate_dwarf_name1_female()
+    else:
+        raise ValueError(
+            f'Valid string parameters for name generating functions are "female", "male", or "any". The given value was the {type(gender).__name__} "{gender}".'
+        )
     return transform_dwarf(name).capitalize()
 
 
 def gen_dwarf_name2() -> str:
     """
-    Outputs a randomized "dwarf" surname. Example outputs:
-    'Stonespear', 'Steelbrow', 'Ironmead', 'Proudhelm', 'Battlemace'
+    Outputs a randomized "dwarf" surname.
     """
     name = gen_from_table(
         compound_tables["mountain_col1"], compound_tables["mountain_col2"]
@@ -37,5 +60,9 @@ def gen_dwarf_name2() -> str:
 # -----------------------------
 
 
-def generate_dwarf_name() -> str:
-    return gen_dwarf_name1() + " " + gen_dwarf_name2()
+def generate_dwarf_name(gender: str = "any") -> str:
+    """
+    Outputs a randomized "dwarf" first and last name, with the first name having
+    variable gender.
+    """
+    return gen_dwarf_name1(gender) + " " + gen_dwarf_name2()
