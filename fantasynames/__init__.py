603d396abaa06615c3e6a5545f6640c63232dbc5
Jessica Peck <jessypeck@gmail.com>
2021-04-04 11:30:18 -0700

    Refactor french to use Language class Also adds gender functionality

--- fantasynames/__init__.py
+++ fantasynames/__init__.py
@@ -1,6 +1,6 @@
 from fantasynames.hobbit import generate_hobbit_name as hobbit
 from fantasynames.elf import elf
-from fantasynames.french import generate_french_name as french
+from fantasynames.french import french
 from fantasynames.anglo import anglo
 from fantasynames.dwarf import dwarf
 from fantasynames.human_diverse import generate_human_name as human
