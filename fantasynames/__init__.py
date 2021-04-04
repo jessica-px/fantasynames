82965159cdfc492d2e8b18cd0d8e9f9ed2fdba19
Jessica Peck <jessypeck@gmail.com>
2021-04-04 09:50:00 -0700

    Use Language class for elf this also adds gender functionality

--- fantasynames/__init__.py
+++ fantasynames/__init__.py
@@ -1,6 +1,6 @@
 from fantasynames.hobbit import generate_hobbit_name as hobbit
-from fantasynames.elf import generate_elf_name as elf
+from fantasynames.elf import elf
 from fantasynames.french import generate_french_name as french
 from fantasynames.anglo import anglo
-from fantasynames.dwarf import generate_dwarf_name as dwarf
+from fantasynames.dwarf import dwarf
 from fantasynames.human_diverse import generate_human_name as human
