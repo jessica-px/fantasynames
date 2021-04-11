6015807b84362ebe68178cb263038a31599e39d8
Jessica Peck <jessypeck@gmail.com>
2021-04-11 09:39:08 -0700

    Add test for # character rule

--- fantasynames/tests/special_rules_test.py
+++ fantasynames/tests/special_rules_test.py
@@ -38,3 +38,15 @@ def test_ampersand_2():
     name = "e" + "r&ic"
     transformed_name = TestLanguage._transform(name)
     assert transformed_name == "eric"
+
+
+def test_pound():
+    name = "and" + "r#e"
+    transformed_name = TestLanguage._transform(name)
+    assert transformed_name == "andre"
+
+
+def test_pound_2():
+    name = "ann" + "r#e"
+    transformed_name = TestLanguage._transform(name)
+    assert transformed_name == "anne"
