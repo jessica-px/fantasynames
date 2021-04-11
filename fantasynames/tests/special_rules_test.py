6813b5aa3f1cfeefe561b887be1cb06f5974501c
Jessica Peck <jessypeck@gmail.com>
2021-04-11 08:48:38 -0700

    Add tests for ampersand rule

--- fantasynames/tests/special_rules_test.py
+++ fantasynames/tests/special_rules_test.py
@@ -26,3 +26,15 @@ def test_male_name():
 def test_female_name():
     name = TestLanguage.name("female")
     assert name == "Jacky the Brave d'Blanche"
+
+
+def test_ampersand():
+    name = "dun" + "r&ic"
+    transformed_name = TestLanguage._transform(name)
+    assert transformed_name == "dunic"
+
+
+def test_ampersand_2():
+    name = "e" + "r&ic"
+    transformed_name = TestLanguage._transform(name)
+    assert transformed_name == "eric"
