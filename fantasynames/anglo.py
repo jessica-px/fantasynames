99b4d9060af52d22f515428085ca26e4b4a1cfdb
leov1963 <leov1963@gmail.com>
2021-04-15 21:02:23 -0700

    Update human_data to anglo_data where present

--- fantasynames/anglo.py
+++ fantasynames/anglo.py
@@ -1,20 +1,20 @@
-from fantasynames.data import human_data
+from fantasynames.data import anglo_data
 from fantasynames.language import Language
 import random
 
 
 class Anglo(Language):
-    transformations = human_data["transformations"]
+    transformations = anglo_data["transformations"]
 
     @classmethod
     def _name1_male(cls) -> str:
         prob = random.random() * 100
         # 50% chance to use col1 + col2
         if prob < 50:
-            cols = [human_data["name1_col1"], human_data["name1_col2"]]
+            cols = [anglo_data["name1_col1"], anglo_data["name1_col2"]]
         # 50% chance to use col1 + a male suffix
         else:
-            cols = [human_data["name1_col1"], human_data["name1_male_suffixes"]]
+            cols = [anglo_data["name1_col1"], anglo_data["name1_male_suffixes"]]
         return cls._name_from_lists(cols)
 
     @classmethod
@@ -22,19 +22,19 @@ class Anglo(Language):
         prob = random.random() * 100
         # 50% chance of just using col1 + female suffix
         if prob < 50:
-            cols = [human_data["name1_col1"], human_data["name1_female_suffixes"]]
+            cols = [anglo_data["name1_col1"], anglo_data["name1_female_suffixes"]]
         # 50% chance of col1 + col2 + female suffix
         else:
             cols = [
-                human_data["name1_col1"],
-                human_data["name1_col2"],
-                human_data["name1_female_suffixes"],
+                anglo_data["name1_col1"],
+                anglo_data["name1_col2"],
+                anglo_data["name1_female_suffixes"],
             ]
         return cls._name_from_lists(cols)
 
     @classmethod
     def _name2(cls) -> str:
-        cols = [human_data["name2_col1"], human_data["name2_col2"]]
+        cols = [anglo_data["name2_col1"], anglo_data["name2_col2"]]
         name = cls._name_from_lists(cols)
         # 50% chance to use "name1 of name2" format
         if random.random() * 100 < 50:
