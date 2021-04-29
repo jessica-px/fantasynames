5677fd9819eb34af1237d907535d8af824bdee1c
leov1963 <leov1963@gmail.com>
2021-04-29 16:19:23 -0700

    Update orc.py: - comment out female name method - add orc = Orc.name - change _name1_male to _name1_any ** Throws type error **

--- fantasynames/orc.py
+++ fantasynames/orc.py
@@ -4,9 +4,11 @@ from fantasynames.data import orc_data
 class Orc(Language):
 
     @classmethod
-    def _name1_male(cls) -> str:
+    def _name1_any(cls) -> str:
         return cls._name_from_lists([orc_data["name1_col1"], orc_data["name1_col2"]])
 
-    @classmethod
-    def _name1_female(cls) -> str:
-        return cls._name1_male()
+    # @classmethod
+    # def _name1_female(cls) -> str:
+    #     return cls._name1_male()
+
+orc = Orc.name
