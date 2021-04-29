a83773fd4b69f97ef55219fbd5330143eae085e1
leov1963 <leov1963@gmail.com>
2021-04-29 15:51:02 -0700

    Add Orc class with name generating logic to orc.py

--- fantasynames/orc.py
+++ fantasynames/orc.py
@@ -1,3 +1,12 @@
 from fantasynames.language import Language
 from fantasynames.data import orc_data
 
+class Orc(Language):
+
+    @classmethod
+    def _name1_male(cls) -> str:
+        return cls._name_from_lists([orc_data["name1_col1"], orc_data["name1_col2"]])
+
+    @classmethod
+    def _name1_female(cls) -> str:
+        return cls._name1_male()
