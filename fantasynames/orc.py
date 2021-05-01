41e5d6dc057d9b477393ae5322c43932b74db9a9
leov1963 <leov1963@gmail.com>
2021-04-30 17:08:27 -0700

    Fix orc.py

--- fantasynames/orc.py
+++ fantasynames/orc.py
@@ -4,11 +4,15 @@ from fantasynames.data import orc_data
 class Orc(Language):
 
     @classmethod
-    def _name1_any(cls) -> str:
+    def _name1_male(cls) -> str:
         return cls._name_from_lists([orc_data["name1_col1"], orc_data["name1_col2"]])
 
-    # @classmethod
-    # def _name1_female(cls) -> str:
-    #     return cls._name1_male()
+    @classmethod
+    def _name1_female(cls) -> str:
+        return cls._name1_male()
+
+    @classmethod
+    def _name2(cls) -> str:
+        return cls._name_from_lists([orc_data["name2_col1"], orc_data["name2_col2"]])
 
 orc = Orc.name
