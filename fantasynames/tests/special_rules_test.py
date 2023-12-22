170b20bfaa1d2154fe3cdfbcbb84af1815a5cd54
Galit Dotan <galitdotan99@gmail.com>
2023-12-22 15:11:46 +0200

    Updated tests

--- fantasynames/tests/special_rules_test.py
+++ fantasynames/tests/special_rules_test.py
@@ -7,7 +7,6 @@ from fantasynames.languages.language import Language
 
 
 class TestLanguage(Language):
-
     def first_name_male(self) -> str:
         return "bob*y"
 
@@ -19,34 +18,34 @@ class TestLanguage(Language):
 
 
 def test_male_name():
-    name = TestLanguage.name(Gender.MALE)
+    name = TestLanguage().name(Gender.MALE)
     assert name == "Bobby the Brave d'Blanche"
 
 
 def test_female_name():
-    name = TestLanguage.name(Gender.FEMALE)
+    name = TestLanguage().name(Gender.FEMALE)
     assert name == "Jacky the Brave d'Blanche"
 
 
 def test_ampersand():
     name = "dun" + "r&ic"
-    transformed_name = TestLanguage._transform(name)
+    transformed_name = TestLanguage()._transform(name)
     assert transformed_name == "dunic"
 
 
 def test_ampersand_2():
     name = "e" + "r&ic"
-    transformed_name = TestLanguage._transform(name)
+    transformed_name = TestLanguage()._transform(name)
     assert transformed_name == "eric"
 
 
 def test_pound():
     name = "and" + "r#e"
-    transformed_name = TestLanguage._transform(name)
+    transformed_name = TestLanguage()._transform(name)
     assert transformed_name == "andre"
 
 
 def test_pound_2():
     name = "ann" + "r#e"
-    transformed_name = TestLanguage._transform(name)
+    transformed_name = TestLanguage()._transform(name)
     assert transformed_name == "anne"
